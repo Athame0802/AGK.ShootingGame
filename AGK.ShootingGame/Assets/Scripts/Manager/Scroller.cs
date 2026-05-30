53482d5864837ca3a03dbe69cb31632fa5f36f1c
Athame0802 <sdh260107@sdh.hs.kr>
2026-05-30 23:27:27 +0900

    feat(Background): 배경 스크롤 기능 추가
    - 적 수에 따른 배경 스크롤 속도 변화 기능 추가

---
+++ AGK.ShootingGame/Assets/Scripts/Manager/Scroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    public static Scroller Instance = default;
+    
+    public int enemyCount { get; set; } = default;
+    public int bossEnemyCount { get; set; } = default;
+
+    private float moveSpeed;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector2(
+            transform.position.x,
+            transform.position.y - moveSpeed * Time.deltaTime);
+    }
+
+    public void ScrollSpeedCheck()
+    {
+        if (enemyCount <= 0 && bossEnemyCount <= 0)
+        {
+            moveSpeed = 1f;
+            return;
+        }
+
+        if (bossEnemyCount > 0)
+        {
+            moveSpeed = 0f;
+        }
+        
+        moveSpeed = 0.75f / enemyCount;
+    }
+}
