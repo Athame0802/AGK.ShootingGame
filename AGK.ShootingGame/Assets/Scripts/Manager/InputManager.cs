2b6d99792051ee959184dfc3c7c319409b81c392
Athame0802 <sdh260107@sdh.hs.kr>
2026-05-30 18:30:02 +0900

    feat(Manager): InputManager 추가
    - 입력 감지를 위한 InputManager 추가

---
+++ AGK.ShootingGame/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance = default;
+    public float HorizontalInput { get; private set; } = default;
+    public float VerticalInput { get; private set; } = default;
+
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        HorizontalInput = Input.GetAxisRaw(HORIZONTAL);
+        VerticalInput = Input.GetAxisRaw(VERTICAL);
+    }
+}
