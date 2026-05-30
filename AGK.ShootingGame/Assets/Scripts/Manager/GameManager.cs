e7c1c1c3236dc1f2886f4586e28fe473d83d66b6
Athame0802 <sdh260107@sdh.hs.kr>
2026-05-30 20:54:48 +0900

    feat(manager): GameManager 추가
    - 전체적 게임의 흐름 관리용 GameManager 클래스 추가
    - 오버헤드 및 실수 방지를 위해 Scene 번호를 담은 Scenes 클래스 추가

---
+++ AGK.ShootingGame/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = default;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(Scenes.Stage1);
+    }
+
+    // TODO : 스테이지 순서를 넣은 SO를 만들어 다음으로 이동 가능하게 하기
+    public void MoveToNextScene()
+    {
+
+    }
+
+    // TODO : Gameover 씬 추가해서 넣기
+    public void GameOver()
+    {
+
+    }   
+}
---
+++ AGK.ShootingGame/Assets/Scripts/Other/Scenes.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class Scenes
+{
+    public static int Stage1 = SceneManager.GetSceneByName("Stage1").buildIndex;
+}
