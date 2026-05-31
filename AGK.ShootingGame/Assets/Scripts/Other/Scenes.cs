f9e2bb94b94292194cc023c4b0dbb022e4db3437
Athame0802 <sdh260107@sdh.hs.kr>
2026-05-31 15:55:56 +0900

    fix(scene): 씬 번호를 가져올 수 없는 문제 해결
    - 메서드의 한계로 인해 현재 활성화된 씬 번호만 가져올 수 있어 enum으로 씬 번호를 하드코딩해 해결

--- AGK.ShootingGame/Assets/Scripts/Other/Scenes.cs
+++ AGK.ShootingGame/Assets/Scripts/Other/Scenes.cs
@@ -1,9 +1,7 @@
-﻿using UnityEngine.SceneManagement;
-
-public static class Scenes
+﻿public enum Scenes
 {
-    public const int LAST_STAGE = 1;
-    public static int Stage1 = SceneManager.GetSceneByName("Stage1").buildIndex;
-    public static int GameOver = SceneManager.GetSceneByName("GameOver").buildIndex;
-    public static int End = SceneManager.GetSceneByName("End").buildIndex;
+    LAST_STAGE = 1,
+    Stage1 = 1,
+    GameOver,
+    End
 }
